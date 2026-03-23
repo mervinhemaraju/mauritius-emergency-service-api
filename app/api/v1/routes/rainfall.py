b79ef283526b183805546f0e79d6bcd343252584
Mervin Hemaraju <th3pl4gu33@gmail.com>
2026-03-23 19:17:40 +0400

    - Removed unused code

--- app/api/v1/routes/rainfall.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-from flask_restful import marshal_with
-
-
-class HeavyRainfallAlertsResource(Resource):
-    # > GET Request
-    def get(self, lang):
-
-        pass
